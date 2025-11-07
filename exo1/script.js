948ab2718c00cf461fc7dfa42bcdd333b25f3e2c
YaniAnis <155096499+YaniAnis@users.noreply.github.com>
2025-11-07 20:05:27 +0100

    TP Js fin

---
+++ exo1/script.js
@@ -0,0 +1,20 @@
+"use strict";
+ // vue dans les slides 33 et 45
+function F1() {
+    console.log("John");
+}
+function F2() {
+    console.log("Doe");
+}
+function F3() {
+    console.log("John Doe");
+}
+function F4() {
+    console.log(30);
+}
+function F5() {
+    console.log(15);
+}
+function F6() {
+    console.log(3.14);
+}
---
+++ exo2/script.js
@@ -0,0 +1,24 @@
+"use strict";
+// vue dans les slides 33 et 45
+function F1() {
+    let etudiants = ["Etudiant1", "Etudiant2", "Etudiant3"];
+    console.log(etudiants);
+}
+
+function F2() {
+    let etudiant = {
+        nom: "John",
+        prenom: "Doe",
+        age: 30
+    };
+    console.log(etudiant);
+}
+
+function F3() {
+    let etudiants = [
+        { nom: "nom1", prenom: "prenom1", age: 21 },
+        { nom: "nom2", prenom: "prenom2", age: 22 },
+        { nom: "nom3", prenom: "prenom3", age: 23 }
+    ];
+    console.log(etudiants);
+}
