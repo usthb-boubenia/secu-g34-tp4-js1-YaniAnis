91e2ac18e7b2d21d7a60bb2764c3635f8e8e4934
YaniAnis <155096499+YaniAnis@users.noreply.github.com>
2025-11-08 10:00:42 +0100

    TP JS part1 Fin

--- exo2/script.js
+++ exo2/script.js
@@ -1,16 +1,16 @@
 "use strict";
-// vue dans les slides 33 et 45
+
 function F1() {
     let etudiants = ["Etudiant1", "Etudiant2", "Etudiant3"];
     console.log(etudiants);
-}
+}// les objets sont vus dans le slide 39
 
 function F2() {
     let etudiant = {
         nom: "John",
         prenom: "Doe",
         age: 30
-    };
+    };// tableau d'objets vue dans le slide 40
     console.log(etudiant);
 }
 
@@ -20,5 +20,7 @@ function F3() {
         { nom: "nom2", prenom: "prenom2", age: 22 },
         { nom: "nom3", prenom: "prenom3", age: 23 }
     ];
-    console.log(etudiants);
+    for (let et of etudiants) {
+        console.log(et.nom + "-" + et.prenom + "-" + et.age);
+    }//boucle, vue dans le slide 44
 }
